5ebf9b1ffc103ceffd9de71643f350f5cc96ee80
Tiago <schmidttiago7@gmail.com>
2023-06-13 21:01:48 -0300

    Adding basic txt data. Adding basic init functions.

--- brasileirao-simulator/src/bin/main.rs
+++ brasileirao-simulator/src/bin/main.rs
@@ -1,63 +1,53 @@
 use std::fs;
 
-use brasileirao_simulator::team::Team;
+use brasileirao_simulator::{game_match::Match, team::Team};
 fn main() {
-    let content = fs::read_to_string("tiago.txt").expect("Deve existir esse arquivo.");
-    let contents = content.split('\n');
+    let team_vec = initialize_team_vec();
+    let match_vec = initialize_match_vec(&team_vec);
+    println!(
+        "Time atual: {:?}",
+        match_vec.first().unwrap().first_team.name
+    );
+}
 
+fn initialize_match_vec(teams_vec: &[Team]) -> Vec<Match> {
+    let mut match_vec: Vec<Match> = Vec::new();
+    let content = fs::read_to_string("../jogos13-06.txt").expect("Deve existir esse arquivo.");
+    for part in content.lines() {
+        let current_match = Match::new(part, teams_vec);
+        match_vec.push(current_match);
+    }
+    match_vec
+}
+
+fn initialize_team_vec() -> Vec<Team> {
     let mut team_vec: Vec<Team> = Vec::new();
+    let content = fs::read_to_string("../times13-06.txt").expect("Deve existir esse arquivo.");
+    for part in content.lines() {
+        let subparts = part.split('>').collect::<Vec<_>>();
+        let name = subparts[78].split('<').next().unwrap();
+        let points: u32 = subparts[81]
+            .split('<')
+            .next()
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+        let wins: u32 = subparts[85]
+            .split('<')
+            .next()
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+        let games: u32 = subparts[83]
+            .split('<')
+            .next()
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
 
-    for (i, part) in contents.enumerate() {
-        let mut name = "";
-        let mut points: u32 = 0;
-        let mut wins: u32 = 0;
-        let mut games: u32 = 0;
-
-        println!("##################################");
-
-        if i % 8 == 0 {
-            let subparts: Vec<&str> = part.split('>').collect();
-            let testes = part.split('>').enumerate();
-            for teste in testes {
-                println!("{:#?}", teste);
-            }
-            name = subparts[11].split('<').next().unwrap();
-            points = subparts[14]
-                .split('<')
-                .next()
-                .unwrap()
-                .parse::<u32>()
-                .unwrap();
-            wins = subparts[18]
-                .split('<')
-                .next()
-                .unwrap()
-                .parse::<u32>()
-                .unwrap();
-            games = subparts[16]
-                .split('<')
-                .next()
-                .unwrap()
-                .parse::<u32>()
-                .unwrap();
-            // for (j, subpart) in part.split('>').enumerate() {
-            //     println!("Loucura:{:#?}", subpart);
-            //     if j == 11 {
-            //         name = subpart.split('<').next().unwrap();
-            //     };
-            //     if j == 14 {
-            //         points = subpart.split('<').next().unwrap().parse::<u32>().unwrap();
-            //     };
-            //     if j == 18 {
-            //         wins = subpart.split('<').next().unwrap().parse::<u32>().unwrap();
-            //     };
-            //     if j == 16 {
-            //         games = subpart.split('<').next().unwrap().parse::<u32>().unwrap();
-            //     };
-            // }
-        }
-        let current_team = Team::new(name.to_string(), points, wins, games, 0.0);
+        let mut current_team = Team::new(name.to_string(), points, wins, games, 0.0);
+        current_team = current_team.update_win_rate();
         team_vec.push(current_team);
     }
-    //println!("Time atual: {:#?}", team_vec);
+    team_vec
 }
--- brasileirao-simulator/src/game_match.rs
+++ brasileirao-simulator/src/game_match.rs
@@ -2,23 +2,21 @@ use crate::team::Team;
 
 #[derive(Debug)]
 pub struct Match<'a> {
-    first_team: &'a Team,
-    second_team: &'a Team,
+    pub first_team: &'a Team,
+    pub second_team: &'a Team,
 }
 
 impl<'a> Match<'a> {
-    pub fn new(match_string: String, teams_array: &'a [Team; 40]) -> Self {
-        let i = 0;
-        let first_team = loop {
-            if teams_array[i].name == "foo" {
-                break &teams_array[i];
-            }
-        };
-        let second_team = loop {
-            if teams_array[i].name == "bar" {
-                break &teams_array[i];
-            }
-        };
+    pub fn new(match_string: &str, teams_vec: &'a [Team]) -> Self {
+        let subparts = match_string.split(' ').collect::<Vec<_>>();
+        let first_team = teams_vec
+            .iter()
+            .find(|item| item.name == subparts[2])
+            .unwrap();
+        let second_team = teams_vec
+            .iter()
+            .find(|item| item.name == subparts[2])
+            .unwrap();
 
         Match {
             first_team,
