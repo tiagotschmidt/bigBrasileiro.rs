7887311e5ca9d2a18f63f34e4b4be7583cfe11d1
Tiago Schmidt <schmidttiago7@gmail.com>
2023-06-16 19:29:05 -0300

    Finding optimal value for thread number.

--- brasileirao-simulator/src/bin/main.rs
+++ brasileirao-simulator/src/bin/main.rs
@@ -1,15 +1,61 @@
 use brasileirao_simulator::{game_match::Match, team::Team};
-use std::fs;
 
-const MAX_SIM: i32 = 1000000;
+use std::fs;
+use std::thread;
+use std::vec;
+
+const MAX_SIM: u32 = 1_000_000;
+const MAX_THREADS: usize = 16;
+const MAX_TEAMS: usize = 20;
 
 fn main() {
-    let mut internacional_positions = [0; 20];
-    let mut internacional_positions_percentage = [0.0; 20];
+    let all_internacional_positions = [[0; MAX_TEAMS]; MAX_THREADS];
+    let mut all_internacional_positions_percentage = [[0.0; MAX_TEAMS]; MAX_THREADS];
+    let mut final_internacional_percentage = [0.0; MAX_TEAMS];
     let match_vec = initialize_match_vec();
     let team_vec = initialize_team_vec();
 
-    for _i in 0..MAX_SIM {
+    let mut thread_vec = vec![];
+
+    for i in 0..MAX_THREADS {
+        let team_vec = team_vec.clone();
+        let match_vec = match_vec.clone();
+        thread_vec.push(thread::spawn(move || {
+            {
+                simulate_championship(
+                    team_vec,
+                    match_vec,
+                    all_internacional_positions[i],
+                    all_internacional_positions_percentage[i],
+                )
+            }
+        }));
+    }
+
+    for (i, handle) in thread_vec.into_iter().enumerate() {
+        all_internacional_positions_percentage[i] = handle.join().unwrap();
+    }
+
+    for (i, _item) in final_internacional_percentage.iter_mut().enumerate() {
+        let mut acc = 0.0;
+
+        for item in all_internacional_positions_percentage.iter() {
+            acc += item[i];
+        }
+
+        *_item = acc;
+    }
+
+    display_result_for_inter("Internacional".to_string(), final_internacional_percentage);
+}
+
+fn simulate_championship(
+    team_vec: Vec<Team>,
+    match_vec: Vec<Match>,
+    mut internacional_positions: [i32; 20],
+    mut internacional_positions_percentage: [f64; 20],
+) -> [f64; 20] {
+    for _i in 0..MAX_SIM / MAX_THREADS as u32 {
         let mut team_vec = team_vec.clone();
 
         for game_match in match_vec.clone() {
@@ -22,15 +68,11 @@ fn main() {
         internacional_positions[internacional_current_position] += 1;
     }
 
-    for i in 0..internacional_positions.len() {
+    for i in 0..MAX_TEAMS {
         internacional_positions_percentage[i] =
             internacional_positions[i] as f64 * 100.0 / MAX_SIM as f64;
     }
-
-    display_result_for_inter(
-        "Internacional".to_string(),
-        internacional_positions_percentage,
-    );
+    internacional_positions_percentage
 }
 
 fn search_team_placement(team_name: String, team_vec: Vec<Team>) -> usize {
@@ -49,8 +91,8 @@ fn search_team_placement(team_name: String, team_vec: Vec<Team>) -> usize {
 
 fn display_result_for_inter(team_name: String, internacional_positions_percentage: [f64; 20]) {
     println!(
-        "###################\tResumo de Simulação com {} repetições\t###################",
-        MAX_SIM
+        "###################\tResumo de Simulação com {} repetições e {} threads\t###################",
+        MAX_SIM, MAX_THREADS
     );
     println!("{}", team_name);
     println!(
