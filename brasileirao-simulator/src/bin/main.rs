6d0566ed36d1a4d5663983db13bf9d2d35bbd71b
Tiago Schmidt <schmidttiago7@gmail.com>
2023-06-16 10:31:55 -0300

    Adding better result display.

--- brasileirao-simulator/src/bin/main.rs
+++ brasileirao-simulator/src/bin/main.rs
@@ -5,32 +5,69 @@ const MAX_SIM: i32 = 1000000;
 
 fn main() {
     let mut internacional_positions = [0; 20];
+    let mut internacional_positions_percentage = [0.0; 20];
     let match_vec = initialize_match_vec();
     let team_vec = initialize_team_vec();
+
     for _i in 0..MAX_SIM {
         let mut team_vec = team_vec.clone();
+
         for game_match in match_vec.clone() {
             team_vec = game_match.simulate_points_game(team_vec);
         }
-
         team_vec.sort_by(|a, b| b.points.cmp(&a.points));
 
-        let mut team_iter = team_vec.iter();
-        let mut team = team_iter.next().unwrap();
-        let mut counter = 0;
-        let internacional_current_position = loop {
-            if team.name == "Internacional" {
-                break counter;
-            }
-            counter += 1;
-            team = team_iter.next().unwrap();
-        };
-
-        //println!("Vetor dos times2: {:#?}", team_vec);
+        let internacional_current_position =
+            search_team_placement("Internacional".to_string(), team_vec);
         internacional_positions[internacional_current_position] += 1;
     }
 
-    println!("Array do inter: {:#?}", internacional_positions);
+    for i in 0..internacional_positions.len() {
+        internacional_positions_percentage[i] =
+            internacional_positions[i] as f64 * 100.0 / MAX_SIM as f64;
+    }
+
+    display_result_for_inter(
+        "Internacional".to_string(),
+        internacional_positions_percentage,
+    );
+}
+
+fn search_team_placement(team_name: String, team_vec: Vec<Team>) -> usize {
+    let mut team_iter = team_vec.iter();
+    let mut team = team_iter.next().unwrap();
+    let mut counter = 0;
+
+    loop {
+        if team.name == team_name {
+            break counter;
+        }
+        counter += 1;
+        team = team_iter.next().unwrap();
+    }
+}
+
+fn display_result_for_inter(team_name: String, internacional_positions_percentage: [f64; 20]) {
+    println!(
+        "###################\tResumo de Simulação com {} repetições\t###################",
+        MAX_SIM
+    );
+    println!("{}", team_name);
+    println!(
+        "Chances de ser Campeão:\t{}%.",
+        internacional_positions_percentage[0]
+    );
+    println!(
+        "Chances de ser Vice Campeão:\t{}%.",
+        internacional_positions_percentage[0]
+    );
+    println!(
+        "Chances de ser Rebaixado:\t{}%.",
+        internacional_positions_percentage[19]
+            + internacional_positions_percentage[18]
+            + internacional_positions_percentage[17]
+            + internacional_positions_percentage[16]
+    )
 }
 
 fn initialize_match_vec() -> Vec<Match> {
