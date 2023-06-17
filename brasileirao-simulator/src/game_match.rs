500c254e31e5eaedd321479387b4e955e6329cd1
Tiago Schmidt <schmidttiago7@gmail.com>
2023-06-17 13:10:58 -0300

    Revert "Adding match history."
    This reverts commit 9ed91ec04d89437e763c29d87a409500d23ca5fc.

--- brasileirao-simulator/src/game_match.rs
+++ brasileirao-simulator/src/game_match.rs
@@ -40,15 +40,10 @@ impl Match {
         (first, second)
     }
 
-    pub fn simulate_points_game(
-        self,
-        mut teams_vec: Vec<Team>,
-        mut match_history: Vec<MatchRegistry>,
-    ) -> (Vec<Team>, Vec<MatchRegistry>) {
+    pub fn simulate_points_game(self, mut teams_vec: Vec<Team>) -> Vec<Team> {
         let mut rng = rand::thread_rng();
         let rng_one: f32 = rng.gen_range(0.0..1.0);
         let rng_two: f32 = rng.gen_range(0.0..1.0);
-        let mut match_status = MatchStatus::Undefined;
 
         let (first_team_index, second_team_index) =
             Match::find_teams(self.first_team, self.second_team, teams_vec.clone());
@@ -70,85 +65,13 @@ impl Match {
 
         let (first_team, second_team) =
             match (first_win_rate >= rng_one, second_win_rate >= rng_two) {
-                (true, false) => {
-                    match_status = MatchStatus::FirstWon;
-                    (first_team.win_points(), second_team.lose())
-                }
-                (false, true) => {
-                    match_status = MatchStatus::SecondWon;
-                    (first_team.lose(), second_team.win_points())
-                }
-                _ => {
-                    match_status = MatchStatus::Tie;
-                    (first_team.tie_points(), second_team.tie_points())
-                }
+                (true, false) => (first_team.win_points(), second_team.lose()),
+                (false, true) => (first_team.lose(), second_team.win_points()),
+                _ => (first_team.tie_points(), second_team.tie_points()),
             };
-
-        if first_team.name == "Internacional" || second_team.name == "Internacional" {
-            match_history.push(MatchRegistry::new(
-                first_team.name.clone(),
-                second_team.name.clone(),
-                match_status,
-            ));
-        };
 
         teams_vec[first_team_index] = first_team;
         teams_vec[second_team_index] = second_team;
-
-        (teams_vec, match_history)
+        teams_vec
     }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct SequenceCounter {
-    match_history: Vec<MatchRegistry>,
-    repeated: u32,
-}
-
-impl SequenceCounter {
-    pub fn new(match_history: Vec<MatchRegistry>) -> Self {
-        SequenceCounter {
-            match_history,
-            repeated: 0,
-        }
-    }
-
-    pub fn raise_counter(&mut self) {
-        self.repeated += 1;
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct MatchRegistry {
-    first_team: String,
-    second_team: String,
-    match_status: MatchStatus,
-}
-
-impl MatchRegistry {
-    pub fn new(first_team: String, second_team: String, match_status: MatchStatus) -> Self {
-        MatchRegistry {
-            first_team,
-            second_team,
-            match_status,
-        }
-    }
-}
-
-impl Default for MatchRegistry {
-    fn default() -> Self {
-        MatchRegistry {
-            first_team: "".to_string(),
-            second_team: "".to_string(),
-            match_status: MatchStatus::Undefined,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum MatchStatus {
-    FirstWon,
-    SecondWon,
-    Tie,
-    Undefined,
 }
