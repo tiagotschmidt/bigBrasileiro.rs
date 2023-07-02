7fbde77fbff491398d713184e5cddb7360b56bd5
Tiago <schmidttiago7@gmail.com>
2023-07-02 00:20:00 -0300

    Updating matches and teams list.

--- brasileirao-simulator/src/bin/main.rs
+++ brasileirao-simulator/src/bin/main.rs
@@ -4,8 +4,8 @@ use std::fs;
 use std::thread;
 use std::vec;
 
-const MAX_SIM: u32 = 1_000_000;
-const MAX_THREADS: usize = 16;
+const MAX_SIM: u32 = 100_000;
+const MAX_THREADS: usize = 8;
 const MAX_TEAMS: usize = 20;
 
 fn main() {
@@ -70,7 +70,7 @@ fn simulate_championship(
 
     for i in 0..MAX_TEAMS {
         internacional_positions_percentage[i] =
-            internacional_positions[i] as f64 * 100.0 / MAX_SIM as f64;
+            internacional_positions[i] as f64 * 100.0 / (MAX_SIM as f64 / MAX_THREADS as f64);
     }
     internacional_positions_percentage
 }
