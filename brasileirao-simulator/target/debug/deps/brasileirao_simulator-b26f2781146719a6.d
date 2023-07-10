299d0fc244f0c625d7cd96b88f6eff9956fff31a
Tiago <schmidttiago7@gmail.com>
2023-07-10 18:35:58 -0300

    Removing target.

--- brasileirao-simulator/target/debug/deps/brasileirao_simulator-b26f2781146719a6.d
+++
@@ -1,8 +0,0 @@
-/home/ximit/Documents/bigBrasileiro.rs/brasileirao-simulator/target/debug/deps/brasileirao_simulator-b26f2781146719a6.rmeta: src/main.rs Cargo.toml
-
-/home/ximit/Documents/bigBrasileiro.rs/brasileirao-simulator/target/debug/deps/brasileirao_simulator-b26f2781146719a6.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- brasileirao-simulator/target/debug/deps/main-897074fb05687e01.d
+++
@@ -1,8 +0,0 @@
-/home/ximit/Documents/bigBrasileiro.rs/brasileirao-simulator/target/debug/deps/main-897074fb05687e01.rmeta: src/bin/main.rs Cargo.toml
-
-/home/ximit/Documents/bigBrasileiro.rs/brasileirao-simulator/target/debug/deps/main-897074fb05687e01.d: src/bin/main.rs Cargo.toml
-
-src/bin/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
